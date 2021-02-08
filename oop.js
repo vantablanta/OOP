e31cfb37c3864aec2307b99bf00a83250421bd2b
michelle <njerimwanjiku@gmail.com>
2021-02-08 16:27:08 +0300

    mixin functiosn to store methods

--- oop.js
+++ oop.js
@@ -52,3 +52,221 @@ function House(numBedrooms) {
   }
   let myHouse = new House(12);
   myHouse instanceof House;
+  
+  function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+    }
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  for (let property in canary){
+      if(canary.hasOwnProperty(property)){
+      ownProps.push(property);
+  }
+}
+ 
+  function Dog(name) {
+    this.name = name;
+  }
+  Dog.prototype.numLegs = 2
+  let beagle = new Dog("Snoopy");
+
+  function Dog(name) {
+    this.name = name;
+  }
+  Dog.prototype.numLegs = 4;
+  let beagle = new Dog("Snoopy");
+  let ownProps = [];
+  let prototypeProps = [];
+  for (let property in beagle){
+    if(beagle.hasOwnProperty(property)){
+      ownProps.push(property);
+    }else{
+      prototypeProps.push(property)
+    }
+  }
+
+  function Dog(name) {
+    this.name = name;
+  }
+  function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog){
+      return true 
+    }else{
+      return false;
+    }
+  
+  
+  }
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    numLegs: 4,
+    eat: function(){
+      console.log("chomp!!")
+    },
+    describe: function(){
+      console.log("My name is" + this.name)
+    }
+  
+  };
+  
+  function Dog(name) {
+    this.name = name;
+  }
+  Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };  
+
+  function Dog(name) {
+    this.name = name;
+  }
+  let beagle = new Dog("Snoopy");
+  Dog.prototype.isPrototypeOf(beagle);
+  
+  function Dog(name) {
+    this.name = name;
+  }
+  let beagle = new Dog("Snoopy");
+  Dog.prototype.isPrototypeOf(beagle);  // yields true
+  // Fix the code below so that it evaluates to true
+  Object.prototype.isPrototypeOf(Dog.prototype);  
+
+  function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+    
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+  };
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function(){
+      console.log("nom nom nom")
+    }
+  
+  };
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype);
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+Dog.prototype= Object.create(Animal.prototype)
+let beagle = new Dog();
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function(){
+    console.log ("Woof!")
+}
+let beagle = new Dog();
+
+function Bird() { }
+Bird.prototype.fly = function() { return "I am flying!"; };
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function (){
+    return "Alas, this is a flightless bird."
+}
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+let glideMixin = function(obj){
+  obj.glide = function(){
+    console.log("Gliding!!")
+  }  
+
+}
+
+glideMixin(boat);
+glideMixin(bird);
+
+function Bird() {
+  let weight = 15;
+  this.getWeight = function(){
+      return weight;
+  }
+ }
+
+ function makeNest() {
+  console.log("A cozy nest is ready");
+}
+makeNest();
+
+
+//immediatly invoked function
+(function(){
+  console.log("A cozy nest is ready");
+})();
+
+
+
+let funModule = (function () {
+  return {
+    isCuteMixin :function(obj) {
+    obj.isCute = function() {
+    return true;
+  };
+  },
+    singMixin : function(obj) {
+    obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+  }
+}
+})();
